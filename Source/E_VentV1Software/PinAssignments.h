16f4798a1ed5b04ef71970528f3eb45380191231
dtquinnde <44785249+dtquinnde@users.noreply.github.com>
2020-04-08 09:35:39 -0600

    Additions to PinAssignments.h

--- Source/E_VentV1Software/PinAssignments.h
+++ Source/E_VentV1Software/PinAssignments.h
@@ -23,4 +23,16 @@ const int ventilatorLCDDB7    = 41;
 const int ALARM_SWITCH_PIN   = 24;
 const int MODE_SWITCH_PIN    = 40;
 
+//Parameter Select Push Buttons
+const int TIDAL_VOLUME_SELECT_PIN 		= 4;
+const int INSPIRATION_TIME_SELECT_PIN	= 5;
+const int BPM_SELECT_PIN				= 6;
+const int THRESHOLD_PRESSURE_SELECT_PIN = 7;
+const int PLATEAU_PAUSE_TIME_SELECT_PIN = 8;
+
+//Parameter Change Encoder Pins
+const int PARAMETER_ENCODER_PIN_1 			= 2;
+const int PARAMETER_ENCODER_PIN_2			= 3;
+const int PARAMETER_ENCODER_PUSH_BUTTON_PIN = 19; 
+
 #endif
