96c18392ab5ee73831d7479b75c58f0ce01f93fa
Calvin Beck <hobbes@ualberta.ca>
2020-04-08 21:47:10 -0400

    Some tidying up of formatting of alarms.cpp.

--- Source/E_VentV1Software/alarms.cpp
+++ Source/E_VentV1Software/alarms.cpp
@@ -73,13 +73,9 @@ uint16_t check_peep(const float pressure) {
 
 
 VentilatorState handle_alarms(VentilatorState state, LiquidCrystal &displayName) {
-
-    if(state.errors){ //There is an unserviced error
-
-       
-
-        //Control the buzzer
-        if(alarmBuzzerTimer > ALARM_SOUND_LENGTH*1000){
+    if (state.errors) { // There is an unserviced error
+        // Control the buzzer
+        if (alarmBuzzerTimer > (ALARM_SOUND_LENGTH*1000)) {
             alarmBuzzerTimer = 0; //Reset the timer
             digitalWrite(ALARM_BUZZER_PIN,!digitalRead(ALARM_BUZZER_PIN)); //Toggle the buzzer output pin
             digitalWrite(ALARM_LED_PIN,!digitalRead(ALARM_LED_PIN));
@@ -87,45 +83,47 @@ VentilatorState handle_alarms(VentilatorState state, LiquidCrystal &displayName)
 
         }
 
-        //Provide the appropriate screen for the error, error flags held in a 16 bit unsigned integer
-        if(state.errors & HIGH_PRESSURE_ALARM){
-            //Display high pressure alarm screen
-             displayHighPressureAlarm(displayName, state.peak_pressure, LCD_MAX_STRING);
+        // Provide the appropriate screen for the error, error flags held in a 16 bit unsigned integer
+        if (state.errors & HIGH_PRESSURE_ALARM) {
+            // Display high pressure alarm screen
+            displayHighPressureAlarm(displayName, state.peak_pressure, LCD_MAX_STRING);
         }
-        else if(state.errors & LOW_PRESSURE_ALARM){
-            //Display low pressure alarm screen
+        else if (state.errors & LOW_PRESSURE_ALARM) {
+            // Display low pressure alarm screen
             displayLowPressureAlarm(displayName, state.peak_pressure, LCD_MAX_STRING);
         }
-        else if(state.errors & HIGH_PEEP_ALARM){
-            //Display high PEEP alarm screen
+        else if (state.errors & HIGH_PEEP_ALARM) {
+            // Display high PEEP alarm screen
             displayHighPEEPAlarm(displayName, state.peep_pressure, LCD_MAX_STRING);
         }
-        else if(state.errors & LOW_PEEP_ALARM){
-            //Display low PEEP alarm screen
+        else if (state.errors & LOW_PEEP_ALARM) {
+            // Display low PEEP alarm screen
             displayLowPEEPAlarm(displayName, state.peep_pressure, LCD_MAX_STRING);
         }
-        else if(state.errors & DISCONNECT_ALARM){
-            //Display disconnect alarm (also a low pressure alarm)
+        else if (state.errors & DISCONNECT_ALARM) {
+            // Display disconnect alarm (also a low pressure alarm)
             displayDisconnectAlarm(displayName);
         }
-        else if(state.errors & HIGH_TEMP_ALARM){
-            //Display high temp alarm screen
+        else if (state.errors & HIGH_TEMP_ALARM) {
+            // Display high temp alarm screen
             displayTemperatureAlarm(displayName, state.controller_temperature, LCD_MAX_STRING);
         }
-        else if(state.errors & APNEA_ALARM){
-            //Display the apnea alarm screen
+        else if (state.errors & APNEA_ALARM) {
+            // Display the apnea alarm screen
             displayApneaAlarm(displayName);
         }
-        else if(state.errors & DEVICE_FAILURE_ALARM){
+        else if (state.errors & DEVICE_FAILURE_ALARM) {
             displayDeviceFailureAlarm(displayName);
             state.machine_state = FailureMode;
-
         }
         else{
+            // TODO: I (Calvin) am actually pretty nervous about this default.
+            // I feel like we should display an unspecified error or something.
+            assert(false);  // This should NOT happen.
             state.errors = 0;
         }
     }
-    else{
+    else {
         alarmBuzzerTimer = 0;
         digitalWrite(ALARM_BUZZER_PIN,LOW);
         digitalWrite(ALARM_LED_PIN,LOW);
