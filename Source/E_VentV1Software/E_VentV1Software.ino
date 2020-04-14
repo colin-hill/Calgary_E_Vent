344a8366ac27c76d4b4a7194896386d15a955b3a
Calvin Beck <hobbes@ualberta.ca>
2020-04-14 19:36:53 -0400

    Clean up whitespace.

--- Source/E_VentV1Software/E_VentV1Software.ino
+++ Source/E_VentV1Software/E_VentV1Software.ino
@@ -58,16 +58,17 @@ float UserParameter::currentBPM = DEFAULT_BPM; //Declaration of user parameter s
 float UserParameter::currentInspirationTime = DEFAULT_INSPIRATION_TIME; // and plateau pause time
 float UserParameter::currentPlateauPauseTime = DEFAULT_PLATEAU_PAUSE_TIME;
 
-UserParameter userParameters[NUM_USER_PARAMETERS] = {UserParameter(MIN_THRESHOLD_PRESSURE, MAX_THRESHOLD_PRESSURE, THRESHOLD_PRESSURE_INCREMENT, THRESHOLD_PRESSURE_SELECT_PIN, DEFAULT_THRESHOLD_PRESSURE, e_ThresholdPressure),
-                                                      UserParameter(MIN_BPM, MAX_BPM, BPM_INCREMENT, BPM_SELECT_PIN, BPM_DEFAULT, e_BPM),
-                                                      UserParameter(MIN_INSPIRATION_TIME, MAX_INSPIRATION_TIME, INSPIRATION_TIME_INCREMENT, INSPIRATION_TIME_SELECT_PIN, DEFAULT_INSPIRATION_TIME,e_InspirationTime),
-                                                      UserParameter(MIN_TIDAL_VOLUME, MAX_TIDAL_VOLUME, TIDAL_VOLUME_INCREMENT, TIDAL_VOLUME_SELECT_PIN, TIDAL_VOLUME_DEFAULT,e_TidalVolume),
-                                                      UserParameter(MIN_PLATEAU_PAUSE_TIME, MAX_PLATEAU_PAUSE_TIME, PLATEAU_PAUSE_TIME_INCREMENT, PLATEAU_PAUSE_TIME_SELECT_PIN, DEFAULT_PLATEAU_PAUSE_TIME,e_PlateauPauseTime),
-                                                      UserParameter(MIN_HIGH_PIP_ALARM, MAX_HIGH_PIP_ALARM, HIGH_PIP_ALARM_INCREMENT, HIGH_PIP_ALARM_SELECT_PIN, HIGH_PIP_ALARM_DEFAULT, e_HighPIPAlarm),
-                                                      UserParameter(MIN_LOW_PIP_ALARM, MAX_LOW_PIP_ALARM, LOW_PIP_ALARM_INCREMENT, LOW_PIP_ALARM_SELECT_PIN, LOW_PIP_ALARM_DEFAULT, e_LowPIPAlarm),
-                                                      UserParameter(MIN_HIGH_PEEP_ALARM, MAX_HIGH_PEEP_ALARM, HIGH_PEEP_ALARM_INCREMENT, HIGH_PEEP_ALARM_SELECT_PIN, HIGH_PEEP_ALARM_DEFAULT, e_HighPEEPAlarm),
-                                                      UserParameter(MIN_LOW_PEEP_ALARM, MAX_LOW_PEEP_ALARM, LOW_PEEP_ALARM_INCREMENT, LOW_PEEP_ALARM_SELECT_PIN, LOW_PEEP_ALARM_DEFAULT, e_LowPEEPAlarm),
-                                                      UserParameter(MIN_LOW_PLATEAU_PRESSURE_ALARM, MAX_LOW_PLATEAU_PRESSURE_ALARM, LOW_PLATEAU_PRESSURE_ALARM_INCREMENT, LOW_PLATEAU_PRESSURE_ALARM_SELECT_PIN, LOW_PLATEAU_PRESSURE_ALARM_DEFAULT, e_LowPlateauPressureAlarm)};
+UserParameter userParameters[NUM_USER_PARAMETERS] =
+   { UserParameter(MIN_THRESHOLD_PRESSURE, MAX_THRESHOLD_PRESSURE, THRESHOLD_PRESSURE_INCREMENT, THRESHOLD_PRESSURE_SELECT_PIN, DEFAULT_THRESHOLD_PRESSURE, e_ThresholdPressure),
+     UserParameter(MIN_BPM, MAX_BPM, BPM_INCREMENT, BPM_SELECT_PIN, BPM_DEFAULT, e_BPM),
+     UserParameter(MIN_INSPIRATION_TIME, MAX_INSPIRATION_TIME, INSPIRATION_TIME_INCREMENT, INSPIRATION_TIME_SELECT_PIN, DEFAULT_INSPIRATION_TIME,e_InspirationTime),
+     UserParameter(MIN_TIDAL_VOLUME, MAX_TIDAL_VOLUME, TIDAL_VOLUME_INCREMENT, TIDAL_VOLUME_SELECT_PIN, TIDAL_VOLUME_DEFAULT,e_TidalVolume),
+     UserParameter(MIN_PLATEAU_PAUSE_TIME, MAX_PLATEAU_PAUSE_TIME, PLATEAU_PAUSE_TIME_INCREMENT, PLATEAU_PAUSE_TIME_SELECT_PIN, DEFAULT_PLATEAU_PAUSE_TIME,e_PlateauPauseTime),
+     UserParameter(MIN_HIGH_PIP_ALARM, MAX_HIGH_PIP_ALARM, HIGH_PIP_ALARM_INCREMENT, HIGH_PIP_ALARM_SELECT_PIN, HIGH_PIP_ALARM_DEFAULT, e_HighPIPAlarm),
+     UserParameter(MIN_LOW_PIP_ALARM, MAX_LOW_PIP_ALARM, LOW_PIP_ALARM_INCREMENT, LOW_PIP_ALARM_SELECT_PIN, LOW_PIP_ALARM_DEFAULT, e_LowPIPAlarm),
+     UserParameter(MIN_HIGH_PEEP_ALARM, MAX_HIGH_PEEP_ALARM, HIGH_PEEP_ALARM_INCREMENT, HIGH_PEEP_ALARM_SELECT_PIN, HIGH_PEEP_ALARM_DEFAULT, e_HighPEEPAlarm),
+     UserParameter(MIN_LOW_PEEP_ALARM, MAX_LOW_PEEP_ALARM, LOW_PEEP_ALARM_INCREMENT, LOW_PEEP_ALARM_SELECT_PIN, LOW_PEEP_ALARM_DEFAULT, e_LowPEEPAlarm),
+     UserParameter(MIN_LOW_PLATEAU_PRESSURE_ALARM, MAX_LOW_PLATEAU_PRESSURE_ALARM, LOW_PLATEAU_PRESSURE_ALARM_INCREMENT, LOW_PLATEAU_PRESSURE_ALARM_SELECT_PIN, LOW_PLATEAU_PRESSURE_ALARM_DEFAULT, e_LowPlateauPressureAlarm) };
 
 volatile boolean alarmReset = false;
 // TODO: These are never set?
@@ -129,7 +130,7 @@ void setup() {
     //LCD Display Startup Message for two seconds
     displayStartupScreen(ventilatorDisplay, softwareVersion, LCD_COLUMNS);
 
-// Set up ventilator state.
+    // Set up ventilator state.
     state = get_init_state();
     state.machine_state = StartupHold;
 
@@ -145,7 +146,7 @@ void setup() {
 
 #ifndef NO_INPUT_DEBUG
     while(StartupHold == state.machine_state) {
-    //Wait for interupt routine triggered by user
+	//Wait for interupt routine triggered by user
 
         //TODO: Declan add interrupt flag to change state
 
@@ -166,28 +167,21 @@ void setup() {
 }
 
 void loop() {
-    //Update LCD*********
-
     state = handle_motor(motorController, state);
-    //state = handle_motor(motorController, state);
 
     //Update the state user input parameters
     state = updateStateUserParameters(state, currentlySelectedParameter, parameterSet, parameterSelectEncoder,
-                       userParameters, NUM_USER_PARAMETERS);
+	userParameters, NUM_USER_PARAMETERS);
 
     //LCD display internal variables and regular screen
-    //displayVentilationScreen(internalTV, internalBPM, internalIERatio, internalThresholdPressure, machineState, peakPressure, plateauPressure, peepPressure);
     if (MotorZeroing == state.machine_state) {
-
         displayHomingScreen(ventilatorDisplay);
-        //displayHomingScreen(alarmDisplay);
     }
     else {
-
         displayUserParameters(currentlySelectedParameter, ventilatorDisplay, state.machine_state, state.vc_state, state.ac_state, state.peak_pressure, state.peep_pressure, LCD_MAX_STRING, userParameters);
     }
+
     //TODO: Add in alarm display
-    
 
     // Read in values for state
     update_state(state);
@@ -200,16 +194,13 @@ void loop() {
 
     }
     else if (BreathLoopStart == state.machine_state) { // BreathLoopStart
-
 #ifdef SERIAL_DEBUG
         Serial.println("Breath Loop Start");
 #endif //SERIAL_DEBUG
 
         state.machine_state = check_mode();
-
         update_motor_settings(state);
     }
-
     else if (ACMode == state.machine_state) {
         state = ac_mode_step(state, userParameters);
     }
@@ -219,6 +210,7 @@ void loop() {
     else if (FailureMode == state.machine_state) {
         state = failure_mode(state);
     }
+
     Serial.print("AC State: ");
     Serial.println(state.ac_state);
     state = handle_alarms(alarmReset, state, alarmDisplay, userParameters, currentlySelectedParameter);
@@ -227,18 +219,16 @@ void loop() {
 //FUNCTIONS
 
 void parameterSetISR() {
-
-    if(parameterSetDebounceTimer > (0.25*S_TO_MS)){
+    if (parameterSetDebounceTimer > (0.25*S_TO_MS)) {
         parameterSetDebounceTimer = 0;
         parameterSet = true;
     }
 }
 
 void alarmResetISR(){
-
-  if(alarmResetDebounceTimer > (0.25*S_TO_MS)){
-    //digitalWrite(ALARM_BUZZER_PIN,HIGH);
-    alarmResetDebounceTimer = 0;
-    alarmReset = true;
-  }
+    if (alarmResetDebounceTimer > (0.25*S_TO_MS)) {
+	//digitalWrite(ALARM_BUZZER_PIN,HIGH);
+	alarmResetDebounceTimer = 0;
+	alarmReset = true;
+    }
 }
--- Source/E_VentV1Software/UserParameter.cpp
+++ Source/E_VentV1Software/UserParameter.cpp
@@ -2,55 +2,55 @@
 
 UserParameter::UserParameter(const float minValue, const float maxValue, const float increment, const uint8_t pin, const float defaultValue, SelectedParameter name){
 	
-	this->minValue = minValue;
-	this->maxValue = maxValue;
-	this->increment = increment;
-	this->selectPin = pin;
-	this->value = defaultValue;
-  this->tmpValue = defaultValue;
-  this->name = name;
+    this->minValue = minValue;
+    this->maxValue = maxValue;
+    this->increment = increment;
+    this->selectPin = pin;
+    this->value = defaultValue;
+    this->tmpValue = defaultValue;
+    this->name = name;
 	
-	return;
+    return;
 }
 
 void UserParameter::updateValue(){
 	
-  this->value = this->tmpValue;
+    this->value = this->tmpValue;
 
-  if(e_BPM == this->name){
-    this->currentBPM = this->value;
-  }
-  else if(e_InspirationTime == this->name){
-    this->currentInspirationTime = this->value;
-  }
-  else if(e_PlateauPauseTime == this->name){
-    this->currentPlateauPauseTime = this->value;
-  }	
+    if(e_BPM == this->name){
+        this->currentBPM = this->value;
+    }
+    else if(e_InspirationTime == this->name){
+        this->currentInspirationTime = this->value;
+    }
+    else if(e_PlateauPauseTime == this->name){
+        this->currentPlateauPauseTime = this->value;
+    }	
 }
 
 void UserParameter::updateTmpValue(int32_t numEncoderSteps){
 
-  if(e_BPM == this->name){
-    this->maxValue = (60.0 / ((2.0*this->currentInspirationTime) + this->currentPlateauPauseTime));
-  }
-  else if(e_InspirationTime == this->name){
-    this->maxValue = (60.0 / (2.0*this->currentBPM)) - ((this->currentPlateauPauseTime)/2);
-  }
+    if(e_BPM == this->name){
+        this->maxValue = (60.0 / ((2.0*this->currentInspirationTime) + this->currentPlateauPauseTime));
+    }
+    else if(e_InspirationTime == this->name){
+        this->maxValue = (60.0 / (2.0*this->currentBPM)) - ((this->currentPlateauPauseTime)/2);
+    }
 	
-	float funcValue = this->tmpValue + (this->increment)*((float)numEncoderSteps);
+    float funcValue = this->tmpValue + (this->increment)*((float)numEncoderSteps);
 	
-	if(funcValue > this->maxValue){
+    if(funcValue > this->maxValue){
 		
-		funcValue = this->maxValue;
+        funcValue = this->maxValue;
 		
-	}
-	else if(funcValue < this->minValue){
+    }
+    else if(funcValue < this->minValue){
 		
-		funcValue = this->minValue;
+        funcValue = this->minValue;
 		
-	}
+    }
 	
-	this->tmpValue = funcValue;
+    this->tmpValue = funcValue;
 	
 }
 
