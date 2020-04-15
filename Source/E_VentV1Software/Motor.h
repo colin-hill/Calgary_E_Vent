01892f142b9fe6ea0df7da0b0cea0063835e3349
shaun-gair <shaun.gair@ucalgary.ca>
2020-04-15 16:26:43 -0600

    magic number update

--- Source/E_VentV1Software/Motor.h
+++ Source/E_VentV1Software/Motor.h
@@ -10,9 +10,9 @@
 //Motor Constants specific to the motor
 //const float QPPR = 17700.6; //Quadrature pulses per revolution
 
-const long int QP_TO_ZEROPOINT = 480; //Quadrature pulses from limit switch to bag edge
+const long int QP_TO_ZEROPOINT = 270; //Quadrature pulses from limit switch to bag edge
 const long int POSITION_TOLERANCE = 50; //Removing this for the time being
-const float QP_AT_FULL_STROKE = 500; //Quadrature pulses at 100% TV that occurs from zeropoint
+const float QP_AT_FULL_STROKE = 680; //Quadrature pulses at 100% TV that occurs from zeropoint
 const float MOTOR_RETURN_FACTOR = 0.2; // Percent of nominal exalation time used to return the motor to zeropoint
 const long int MOTOR_ZEROING_SPEED = 270;
 const long int MOTOR_HOMING_SPEED = MOTOR_ZEROING_SPEED; //QPPS for homing
