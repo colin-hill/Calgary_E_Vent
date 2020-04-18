f730f3ed338b77ffda8577bb7cb21f202229acb4
shaun-gair <shaun.gair@ucalgary.ca>
2020-04-18 17:06:43 -0600

    Update Motor.h

--- Source/E_VentV1Software/Motor.h
+++ Source/E_VentV1Software/Motor.h
@@ -13,10 +13,10 @@
 const long int QP_TO_ZEROPOINT = 220; //Quadrature pulses from limit switch to bag edge
 const long int POSITION_TOLERANCE = 50; //Removing this for the time being
 const float QP_AT_FULL_STROKE = 735; //Quadrature pulses at 100% TV that occurs from zeropoint
-const float MOTOR_RETURN_FACTOR = 0.2; // Percent of nominal exalation time used to return the motor to zeropoint
+const float MOTOR_RETURN_FACTOR = 0.3; // Percent of nominal exalation time used to return the motor to zeropoint
 const long int MOTOR_ZEROING_SPEED = 270;
 const long int MOTOR_HOMING_SPEED = MOTOR_ZEROING_SPEED; //QPPS for homing
-const float ZEROING_TIME = 4.0; // TODO set real time
+const float ZEROING_TIME = 2.0; // TODO set real time
 const float HOMING_TIMEOUT = 10.0;//
 
 const long int ACCEL = 500000;
@@ -28,7 +28,7 @@ const int MOTOR_CONTROLLER_TIMEOUT = 10000;
 
 const float INERTIA_BUFFER = 0.1; //Seconds; The motor has inertia, we allow extra time for it too start and stop
 const float HOMING_BUFFER = 0.25; //Seconds
-const float ZEROING_BUFFER = QP_TO_ZEROPOINT/MOTOR_ZEROING_SPEED + INERTIA_BUFFER; //Wait time to reach zero point
+//const float ZEROING_BUFFER = QP_TO_ZEROPOINT/MOTOR_ZEROING_SPEED + INERTIA_BUFFER; //Wait time to reach zero point
 
 
 #define MOTOR_ADDRESS 0x80 //Set on the RoboClaw controller via Basic Micro Motion Studio
